2525a3af61e8b365dd900aaa3a9afe904bc1fd2a
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-11 16:42:36 -0700

    final model update today

--- src/model.py
+++ src/model.py
@@ -19,6 +19,8 @@ class Model:
     def step(self):
         self.age_scientists()
         self.birth_new_scientists()
+        self.birth_new_ideas()
+        self.set_perceived_rewards
 
     def age_scientists(self):
         for sci in self.scientist_list:
@@ -32,6 +34,21 @@ class Model:
             new_scientist.idea_max_var = poisson(lam=self.perceived_max_var)
             new_scientist.idea_mean_var = poisson(lam=self.perceived_mean_var)
             new_scientist.start_effort = poisson(lam=self.start_effort_mean)
-            
+            self.scientist_list.append(new_scientist)
+
+    def birth_new_ideas(self):
+        for i in range(self.ideas_per_time):
             new_idea = idea.Idea()
             new_idea.idea_mean = poisson(lam=self.idea_mean)
+            new_idea.idea_max = poisson(lam=self.idea_max)
+            new_idea.idea_k = poisson(lam=self.k_mean)
+            new_idea.total_effort = 0
+            new_idea.num_k = 0
+            self.idea_list.append(new_idea)
+
+    def set_perceived_rewards(self):
+        for i in range(self.scientist_list):
+            scientist = self.scientist_list[i]
+            for j in range(self.idea_list):
+                idea = self.idea_list[i]
+
