c8c81cc3dcb23a8b34766bb355113ca807a1f4a0
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-11 15:43:49 -0700

    latest

--- src/scientist.py
+++ src/scientist.py
@@ -1,5 +1,17 @@
+import pandas as pd
+import numpy as np
+
 class Scientist:
     def __init__(self):
+        self.age = 0
+        self.learning_speed = None
+        self.idea_max_var = None
+        self.idea_mean_var = None
+        self.start_effort = None
+        self.available_effort = None
+        self.perceived_rewards = pd.DataFrame(columns=['Idea Mean', 'Idea Max', 'Idea K'])
+        self.idea_effort_tp = []
+        self.idea_eff_tot = []
         return
 
     def step(self):
