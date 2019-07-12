e29389cf66efa06f09a4424d14259c7e5ace1150
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-12 10:42:45 -0700

    model update

--- src/model.py
+++ src/model.py
@@ -1,7 +1,6 @@
 import idea
 import scientist
 import pandas as pd
-from numpy.random import poisson
 
 
 class Model:
@@ -26,7 +25,7 @@ class Model:
         self.birth_new_ideas()
         self.set_perceived_rewards()
         # optimize choice
-        self.update_objects
+        self.update_objects()
 
     # adds one year to the age of every scientist that already exists within the model
     def age_scientists(self):
@@ -47,18 +46,32 @@ class Model:
 
     # loop through every scientist, appending their perceived rewards dataframe with new ideas
     def set_perceived_rewards(self):
-        for i in self.scientist_list:
-            scientist = self.scientist_list[i]
+        for s in self.scientist_list:
+            scientist = self.scientist_list[s]
             df = scientist.perceived_rewards
-            for j in self.idea_list:
-                idea = self.idea_list[j]
-                if j not in df.index:
-                    sci_mult_max = None # random number from ND
-                    sci_mult_mean = None # random number from ND
+            for i in self.idea_list:
+                idea = self.idea_list[i]
+                if i not in df.index:
+                    sci_mult_max = None  # random number from ND
+                    sci_mult_mean = None  # random number from ND
                     idea_mean = sci_mult_mean * idea.idea_mean
                     idea_max = sci_mult_max * idea.idea_max
                     idea_k = scientist.learning_speed * idea.idea_k
-                    df.append(j, idea_mean, idea_max, idea_k)
+                    df.append(i, idea_mean, idea_max, idea_k)
+
+        # optimization call will go here
 
     # loop through each idea object, updating the effort that was invested in this time period
     def update_objects(self):
+        for i in self.idea_list:
+            effort_invested_tp = 0
+            k_paid_tp = 0
+            idea = self.idea_list[i]
+            for s in self.scientist_list:
+                scientist = self.scientist_list[s]
+                effort_invested_tp += scientist.idea_effort_tp[i]
+                k_paid_tp += scientist.k_paid_tp[i]
+            idea.total_effort += effort_invested_tp
+            idea.effort_by_tp.append(effort_invested_tp)
+            idea.num_k += k_paid_tp
+            idea.num_k_by_tp.append(k_paid_tp)
