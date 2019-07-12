cdb4dc34b2bdaefe094b52fa4590496d409354f0
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-12 09:08:25 -0700

    comments

--- src/model.py
+++ src/model.py
@@ -4,6 +4,8 @@ from numpy.random import poisson
 
 
 class Model:
+    # initiates the key parameters within the model, as set in config
+    # creates empty lists to track scientists and ideas; index indicates age
     def __init__(self, config):
         self.num_sci = config.sci_rate
         self.time_periods = config.time_periods
@@ -16,16 +18,19 @@ class Model:
         self.scientist_list = []
         self.idea_list = []
 
+    #  defines the process for one time period within the model
     def step(self):
         self.age_scientists()
         self.birth_new_scientists()
         self.birth_new_ideas()
         self.set_perceived_rewards
 
+    # adds one year to the age of every scientist that already exists within the model
     def age_scientists(self):
         for sci in self.scientist_list:
             sci.age = sci.age + 1
 
+    # creates new scientists, birthed at age 0, and sets their random constants (variances, learning speed, and effort
     def birth_new_scientists(self):
         for i in range(self.num_sci):
             new_scientist = scientist.Scientist()
@@ -36,6 +41,7 @@ class Model:
             new_scientist.start_effort = poisson(lam=self.start_effort_mean)
             self.scientist_list.append(new_scientist)
 
+    # creates new ideas and sets their random constants (true mean, true max, investment cost)
     def birth_new_ideas(self):
         for i in range(self.ideas_per_time):
             new_idea = idea.Idea()
--- src/scientist.py
+++ src/scientist.py
@@ -5,16 +5,16 @@ import numpy as np
 class Scientist:
     def __init__(self, model):
         self.model = model
-        self.age
-        self.learning_speed
-        self.idea_max_var
-        self.idea_mean_var
-        self.start_effort
-        self.available_effort
-        self.perceived_rewards = pd.DataFrame(columns=['Idea Mean', 'Idea Max', 'Idea K'])
-        self.ideas_k_paid = []
-        self.idea_effort_tp = []
-        self.idea_eff_tot = []
+        self.age  # age of the given scientists, initiated at 0 years when entered into model
+        self.learning_speed  # multiplier determining scientist specific K (idea K * learning speed = specific k)
+        self.idea_max_var  # variance determining max perceived returns
+        self.idea_mean_var  # variance determining perceived lambda
+        self.start_effort  # determines starting effort for a scientist in all periods
+        self.available_effort  # counter that determines how much effort a scientist has left to allocate within TP
+        self.perceived_rewards = pd.DataFrame(columns=['Idea Mean', 'Idea Max', 'Idea K'])  # tracks perceived rewards
+        self.ideas_k_paid = []  # records which ideas the scientist has paid the investment cost for
+        self.idea_effort_tp = []  # tracks the effort to be invested across different ideas within time period
+        self.idea_eff_tot = []  # tracks the total effort invested in each idea by the scientist
         return
 
     def step(self):
